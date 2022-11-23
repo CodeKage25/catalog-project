4ac411c44ca242ec0b07310b1e6df7fb2d5e1968
Draq <boomjay008@gmail.com>
2022-11-23 12:37:25 +0100

    feat: added a new function to sort with multiple filters

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["plugin:@next/next/recommended", "@madebywild/eslint-config"],
+  overrides: [
+    {
+      files: ["**/*.d.ts", "./src/pages/**/*.ts?(x)"],
+      rules: {
+        "import/no-default-export": "off",
+      },
+    },
+  ],
+  rules: {
+    "@next/next/no-img-element": "off",
+    "react/no-unknown-property": [
+      "warn",
+      {
+        ignore: ["css", "tw"],
+      },
+    ],
+  },
+};
--- pages/sort.tsx
+++ pages/sort.tsx
@@ -7,25 +7,44 @@ export default function Sort () {
     return matchSorter(cardData, filterParam, {keys: ['price']})
   }
   const [data, setData] = useState()
+
   useEffect(() => {
     const result = sorter(350)
     // @ts-ignore
-    setData(result)
-    console.log(data)
+    // setData(result)
+    setData(fuzzySearchMultipleWords(cardData, ['price', "bedroom"], "375 3"))
   }, [])
   console.log(data)
+
+  function fuzzySearchMultipleWords(
+  rows:any, // This is the data that we want to search
+  keys:any, // this keys is an array of strings that determine what field to be searched through
+  filterValue: string, // This is the value that we want to search might be a simple string or multiple strings
+) {
+  if (!filterValue || !filterValue.length) {
+    return rows
+  }
+
+  const terms = filterValue.split(' ')
+  if (!terms) {
+    return rows
+  }
+  return terms.reduceRight(
+    (results, term) => matchSorter(results, term, {keys}),
+    rows,
+  )
+}
   return (
     <>
-      
       {
         // @ts-ignore
-      data?.map((data, id) => {
-        <div key={id}>
-        {data.price}
-      </div>
-      })
-    }
-    <div>sort</div>
+        data?.map((data, id) => (
+          <div key={id}>
+            {data.price}, {data.bedroom}
+            <img src={data.image} alt={data.name} width="100%" />
+          </div>
+        ))
+      }
     </>
-  ) 
+  )
 }
